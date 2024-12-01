715869fcc58d3a8eb64b8598510ad8c6437e45b4
Petr Portnov <mrjarviscraft@gmail.com>
2024-12-01 15:17:35 +0300

    feat(plugins/none-ls): use `nixfmt-rfc-style` for Nix

--- config/languages/nix.nix
+++ config/languages/nix.nix
@@ -1,1 +1,9 @@
-{ plugins.none-ls.sources.formatting.nixfmt.enable = true; }
+{ pkgs, ... }: {
+  plugins = {
+    lsp.servers.nil_ls.enable = true;
+    none-ls.sources.formatting.nixfmt = {
+      enable = true;
+      package = pkgs.nixfmt-rfc-style;
+    };
+  };
+}
