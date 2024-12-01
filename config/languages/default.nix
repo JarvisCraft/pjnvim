7ae670b5461d441e057a471f671e8a5c8c054b29
Petr Portnov <mrjarviscraft@gmail.com>
2024-12-01 21:08:10 +0300

    feat(config/languages): unflatten structure

--- config/languages/default.nix
+++ config/languages/default.nix
@@ -4,15 +4,6 @@
     ./treesitter.nix
     ./none-ls.nix
     ./lsp-lines.nix
-    # Languages
-    ./shell.nix
-    ./lua.nix
-    ./nix.nix
-    ./rust.nix
-    ./java.nix
-    ./markdown.nix
-    ./yaml.nix
-    ./tex.nix
-    ./ansible.nix
+    ./languages
   ];
 }
---
+++ config/languages/languages/default.nix
@@ -0,0 +1,13 @@
+{
+  imports = [
+    ./ansible.nix
+    ./java.nix
+    ./lua.nix
+    ./markdown.nix
+    ./nix.nix
+    ./rust.nix
+    ./shell.nix
+    ./tex.nix
+    ./yaml.nix
+  ];
+}
