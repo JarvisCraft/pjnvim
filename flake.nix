4a87530a9b14de3533aee7bc7fbefc38b7fce11b
Petr Portnov <mrjarviscraft@gmail.com>
2024-12-02 22:55:08 +0300

    ci(git-hooks): configure initially via flakes

--- flake.nix
+++ flake.nix
@@ -14,26 +14,53 @@
         flake-parts.follows = "flake-parts";
       };
     };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, nixvim, ... }:
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      git-hooks,
+      nixvim,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-      perSystem = { pkgs, system, ... }:
+      imports = [ git-hooks.flakeModule ];
+      perSystem =
+        {
+          pkgs,
+          system,
+          config,
+          ...
+        }:
         let
           module = {
             inherit pkgs;
             module = ./config;
           };
           nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule module;
-        in {
-          checks.default =
-            nixvim.lib.${system}.check.mkTestDerivationFromNixvimModule module;
+        in
+        {
+          checks.default = nixvim.lib.${system}.check.mkTestDerivationFromNixvimModule module;
           packages = {
             nvim = nvim;
             default = nvim;
           };
           formatter = pkgs.nixfmt-rfc-style;
+          pre-commit.settings.hooks = {
+            nixfmt-rfc-style.enable = true;
+            nil.enable = true;
+            stylua.enable = true;
+          };
+          devShells.default = config.pre-commit.devShell;
         };
     };
 }
