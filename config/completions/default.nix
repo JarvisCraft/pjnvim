7e8b6b0c25eaf68689f9ea60c18183b48e087483
Petr Portnov <mrjarviscraft@gmail.com>
2024-12-17 02:18:58 +0300

    feat(plugins/cmp): rework completion keys

--- config/completions/default.nix
+++ config/completions/default.nix
@@ -9,49 +9,89 @@
           { name = "buffer"; }
           { name = "async_path"; }
           { name = "git"; }
-          { name = "cmdline"; }
           { name = "emoji"; }
           { name = "fish"; }
           { name = "latex_symbols"; }
-          { name = "vimtex"; }
+          { name = "crates"; }
+          { name = "nvim_lua"; }
         ];
         window = {
           completion.border = "rounded";
           documentation.border = "rounded";
         };
         mapping = {
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          # Basics
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ select = false })";
+          # Completion choice and snippet argument jumps
           "<C-j>" = "cmp.mapping.select_next_item()";
           "<C-k>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.close()";
+          "<Tab>" = ''
+            cmp.mapping(
+                function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    else
+                        local luasnip = require('luasnip')
+                        if luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end
+                end,
+                {'i', 's'}
+            )
+          '';
+          "<S-Tab>" = ''
+            cmp.mapping(
+                function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    else
+                        local luasnip = require('luasnip')
+                        if luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end
+                end,
+                {'i', 's'}
+            )
+          '';
+          # Docs navigation
           "<C-d>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          # https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#safely-select-entries-with-cr
-          "<CR>" = ''
-            cmp.mapping({
-              i = function(fallback)
-                if cmp.visible() and cmp.get_active_entry() then
-                  cmp.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = false,
-                  })
-                else
-                  fallback()
-                end
-              end,
-              s = cmp.mapping.confirm({ select = true }),
-              c = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Replace,
-                select = true
-              }),
-            })
-          '';
+          # Close
           "<Esc>" = "cmp.mapping.abort()";
+          "<C-e>" = "cmp.mapping.close()";
         };
         snippet.expand = "luasnip";
+        experimental.ghost_text = true;
       };
+      filetype =
+        let
+          texSources = [
+            { name = "buffer"; }
+            { name = "async_path"; }
+            { name = "luasnip"; }
+            { name = "vimtex"; }
+            {
+              name = "latex_symbols";
+              option.strategy = 2;
+            }
+          ];
+        in
+        {
+          tex.sources = texSources;
+          latex.sources = texSources;
+          gitcommit.sources = [
+            { name = "buffer"; }
+            { name = "async_path"; }
+            { name = "conventionalcommits"; }
+          ];
+        };
     };
     lspkind.enable = true;
   };
---
+++ config/languages/signature.nix
@@ -0,0 +1,11 @@
+{
+  plugins.lsp-signature = {
+    enable = true;
+    settings = {
+      extra_trigger_chars = [
+        "("
+        ","
+      ];
+    };
+  };
+}
