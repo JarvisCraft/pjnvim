bb9ac5e83f2e1e237d4536aae85dd4d414b01593
Petr Portnov <mrjarviscraft@gmail.com>
2024-12-22 17:13:27 +0300

    feat(modules/languages): configure `statix`

--- config/languages/languages/nix.nix
+++ config/languages/languages/nix.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   plugins = {
     lsp.servers.nil_ls.enable = true;
-    none-ls.sources.formatting.nixfmt = {
-      enable = true;
-      package = pkgs.nixfmt-rfc-style;
+    none-ls.sources = {
+      formatting.nixfmt = {
+        enable = true;
+        package = pkgs.nixfmt-rfc-style;
+      };
+      code_actions.statix.enable = true;
+      diagnostics.statix.enable = true;
     };
   };
 }
