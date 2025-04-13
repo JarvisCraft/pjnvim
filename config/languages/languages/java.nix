96400ee0ef59efaacbe46930789512dc4b757000
Petr Portnov <mrjarviscraft@gmail.com>
2025-04-13 16:43:53 +0300

    chore(deps): update on 13.04.2025

--- config/languages/languages/java.nix
+++ config/languages/languages/java.nix
@@ -1,24 +1,27 @@
 { pkgs, lib, ... }:
 {
-  plugins.nvim-jdtls = {
-    enable = true;
-    cmd = [
-      "${pkgs.jdt-language-server}/bin/jdtls"
-      "--jvm-arg=-javaagent:${pkgs.lombok}/share/java/lombok.jar"
-      # "-configuration"
-      # "/dev/null"
-    ];
+  plugins = {
+    jdtls = {
+      enable = true;
+      lazyLoad.settings.ft = "java";
+      settings = {
+        cmd = [
+          (lib.getExe pkgs.jdt-language-server)
+          "--jvm-arg=-javaagent:${pkgs.lombok}/share/java/lombok.jar"
+        ];
 
-    rootDir = lib.nixvim.mkRaw "require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'})";
-    settings.java = {
-      signatureHelp = true;
-      completion = true;
+        java = {
+          signatureHelp = true;
+          completion = true;
+        };
+        initOptions.bundles = [
+          "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server/com.microsoft.java.debug.plugin-*.jar"
+          "${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server/com.microsoft.java.test.plugin-*.jar"
+          "${pkgs.vscode-extensions.vscjava.vscode-maven}/share/vscode/extensions/vscjava.vscode-maven/jdtls.ext/com.microsoft.java.maven.plugin/target/com.microsoft.java.maven.plugin-*.jar"
+          "${pkgs.vscode-extensions.vscjava.vscode-gradle}/share/vscode/extensions/vscjava.vscode-gradle/server/com.microsoft.gradle.bs.importer-*.jar"
+        ];
+      };
     };
-    initOptions.bundles = [
-      "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server/com.microsoft.java.debug.plugin-0.50.0.jar"
-      "${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server/com.microsoft.java.test.plugin-0.40.1.jar"
-      "${pkgs.vscode-extensions.vscjava.vscode-maven}/share/vscode/extensions/vscjava.vscode-maven/jdtls.ext/com.microsoft.java.maven.plugin/target/com.microsoft.java.maven.plugin-0.39.0.jar"
-      "${pkgs.vscode-extensions.vscjava.vscode-gradle}/share/vscode/extensions/vscjava.vscode-gradle/server/com.microsoft.gradle.bs.importer-0.2.0.jar"
-    ];
+    none-ls.sources.diagnostics.checkstyle.enable = true;
   };
 }
