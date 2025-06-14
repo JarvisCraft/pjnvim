d91381142a16378457d83b63c243104bcadf5d05
Petr Portnov <mrjarviscraft@gmail.com>
2025-06-14 12:21:56 +0300

    feat(modules/languages): add `docker`

--- config/languages/languages/docker-compose.nix
+++
@@ -1,3 +0,0 @@
-{
-  plugins.lsp.servers.docker_compose_language_service.enable = true;
-}
---
+++ config/languages/languages/docker.nix
@@ -0,0 +1,6 @@
+{
+  plugins.lsp.servers = {
+    dockerls.enable = true;
+    docker_compose_language_service.enable = true;
+  };
+}
