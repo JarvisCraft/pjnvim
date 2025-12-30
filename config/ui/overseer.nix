91fc017e6612e978ceaac316b51e0b7c7d80023e
Petr Portnov <mrjarviscraft@gmail.com>
2025-12-30 21:27:49 +0300

    feat(overseer): add keybindings

--- config/ui/overseer.nix
+++ config/ui/overseer.nix
@@ -1,1 +1,35 @@
-{ plugins.overseer.enable = true; }
+{
+  plugins.overseer.enable = true;
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>or";
+      action = "<cmd>OverseerRun<CR>";
+      options.desc = "Run task";
+    }
+    {
+      mode = "n";
+      key = "<leader>oo";
+      action = "<cmd>OverseerOpen<CR>";
+      options.desc = "Open tasks configuration";
+    }
+    {
+      mode = "n";
+      key = "<leader>od";
+      action = "<cmd>OverseerClose<CR>";
+      options.desc = "Close tasks configuration";
+    }
+    {
+      mode = "n";
+      key = "<leader>os";
+      action = "<cmd>OverseerShell<CR>";
+      options.desc = "Run shell command";
+    }
+    {
+      mode = "n";
+      key = "<leader>to";
+      action = "<cmd>OverseerToggle<CR>";
+      options.desc = "Toggle tasks configuration";
+    }
+  ];
+}
