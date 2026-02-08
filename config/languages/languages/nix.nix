08dfc8e32ea04b60d5c9c06095693ff4efb504a4
Petr Portnov <mrjarviscraft@gmail.com>
2026-02-08 15:29:05 +0300

    feat(languages/nix): don't redundantly override fmt package

--- config/languages/languages/nix.nix
+++ config/languages/languages/nix.nix
@@ -1,12 +1,8 @@
-{ pkgs, ... }:
 {
   plugins = {
     lsp.servers.nil_ls.enable = true;
     none-ls.sources = {
-      formatting.nixfmt = {
-        enable = true;
-        package = pkgs.nixfmt;
-      };
+      formatting.nixfmt.enable = true;
       code_actions.statix.enable = true;
       diagnostics = {
         statix.enable = true;
